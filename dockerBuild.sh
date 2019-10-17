3d425caaa472c5412c0861915716b4555922f494
Dewi Jones <dewijones92@gmail.com>
2019-10-17 10:47:35 +0000

    Add docker build script

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -6,6 +6,8 @@ SECRET_DIR="/home/vagrant/release_key"
 PROJECT_DIR="/project"
 RELEASE_KEY_FILENAME="my-release-key.jks"
 KEYSTORE_FILE_NAME="keystore.properties"
+GIT_SECRET_FILE_NAME="secrets.properties"
+
 echo "111"
 echo "$SECRET_DIR/$RELEASE_KEY_FILENAME"
 echo "222"
@@ -17,6 +19,7 @@ echo "222"
 docker run -it -d --name $CONTAINER_NAME \
 -v "$SECRET_DIR/$RELEASE_KEY_FILENAME":"$PROJECT_DIR/$RELEASE_KEY_FILENAME":ro \
 -v "$SECRET_DIR/$KEYSTORE_FILE_NAME":"$PROJECT_DIR/$KEYSTORE_FILE_NAME":ro \
+-v "$SECRET_DIR/$GIT_SECRET_FILE_NAME":"$PROJECT_DIR/$GIT_SECRET_FILE_NAME":ro \
 -v gradle:"/root/.gradle" -v `pwd`:$PROJECT_DIR jangrewe/gitlab-ci-android  bash
 
 docker exec -it $CONTAINER_NAME bash -c 'export GRADLE_USER_HOME=/root/.gradle; cd /project; chmod +x ./gradlew; echo "KEYSTOREPROPS" ; cat keystore.properties; ./gradlew clean; ./gradlew check; ./gradlew build' 
