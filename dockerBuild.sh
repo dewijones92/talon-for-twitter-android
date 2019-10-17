557aa04435e1414b3ff7d0382ba4240d8a32c293
Dewi Jones <dewijones92@gmail.com>
2019-10-17 09:32:33 +0000

    Add docker build script

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -4,7 +4,7 @@
 CONTAINER_NAME="docker_android_build"
 SECRET_DIR="/home/vagrant/release_key"
 PROJECT_DIR="/project"
-RELEASE_KEY_FILENAME="my-release_key.jks"
+RELEASE_KEY_FILENAME="my-release-key.jks"
 KEYSTORE_FILE_NAME="keystore.properties"
 echo "111"
 echo "$SECRET_DIR/$RELEASE_KEY_FILENAME"
@@ -12,11 +12,11 @@ echo "222"
 
 
 #	-v "$SECRET_DIR/$RELEASE_KEY_FILENAME":"$PROJECT_DIR/$RELEASE_KEY_FILENAME":ro \
-#	-v "$SECRET_DIR/$KEYSTORE_FILE_NAME":$PROJECT_DIR/$KEYSTORE_FILE_NAME:ro \
+#	-v "$SECRET_DIR/$KEYSTORE_FILE_NAME":"$PROJECT_DIR/$KEYSTORE_FILE_NAME":ro \
 
 docker run -it -d --name $CONTAINER_NAME \
-	-v "$SECRET_DIR/$RELEASE_KEY_FILENAME":"$PROJECT_DIR/$RELEASE_KEY_FILENAME":ro \
-	-v "$SECRET_DIR/$KEYSTORE_FILE_NAME":$PROJECT_DIR/$KEYSTORE_FILE_NAME:ro \
-	-v gradle:"/root/.gradle" -v `pwd`:$PROJECT_DIR jangrewe/gitlab-ci-android  bash
+-v "$SECRET_DIR/$RELEASE_KEY_FILENAME":"$PROJECT_DIR/$RELEASE_KEY_FILENAME":ro \
+-v "$SECRET_DIR/$KEYSTORE_FILE_NAME":"$PROJECT_DIR/$KEYSTORE_FILE_NAME":ro \
+-v gradle:"/root/.gradle" -v `pwd`:$PROJECT_DIR jangrewe/gitlab-ci-android  bash
 
 docker exec -it $CONTAINER_NAME bash -c 'export GRADLE_USER_HOME=/root/.gradle; cd /project; chmod +x ./gradlew; echo "KEYSTOREPROPS" ; cat keystore.properties; ./gradlew clean; ./gradlew check; ./gradlew build' 
